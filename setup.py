3d4485e4865005e690d55c8d6bd6ca0d7bbf9260
Karthik <karthikeyan.arunagiri@gmail.com>
2026-04-12 22:56:10 +0530

    Setup File added and libraries are installed

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hyphen_E_Dot ='-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\"", "") for req in requirements]
+
+        if Hyphen_E_Dot in requirements:
+            requirements.remove(Hyphen_E_Dot)
+    return requirements
+
+
+setup(
+    name='student-performance',
+    version ='0.0.1',
+    author='karthik',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
